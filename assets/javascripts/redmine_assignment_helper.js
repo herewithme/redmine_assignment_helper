b49e9641ef45a4f567f596c52d30a613ce93154c
Amaury Balmer <amaury@beapi.fr>
2014-04-13 09:53:03 +0200

    reformat code style

--- assets/javascripts/redmine_assignment_helper.js
+++ assets/javascripts/redmine_assignment_helper.js
@@ -1,35 +1,35 @@
-$(function(){
-	// Append dialog on footer
-	$("body").append('<div id="dialog-confirm" style="display:none;" title="Are you sure ?">' 
-				+ '<p><span class="ui-icon ui-icon-alert" style="float:left; margin:0 7px 20px 0;"></span>'
-				+ 'Create a ticket with no assigned person, not alert person (no email). Your ticket may go to the trap-door. We recommend that you set one!</p>'
-				+ '</div>' );
+$(function () {
+    // Append dialog on footer
+    $("body").append('<div id="rah-dialog" style="display:none;" title="Are you sure ?"><p><span class="ui-icon ui-icon-alert" style="float:left; margin:0 7px 40px 0;"></span><span id="dynamic-text"></span></p></div>');
 
-	// Hook form submission
-	$( "input[name='commit'], input[name='continue']" ).click(function( event ) {
-		// Get form related
-		var the_issue_form = $(event.target).closest("form");
+    // Hook form submission
+    $("input[name='commit'], input[name='continue']").click(function (event) {
+        // Get form related
+        var the_issue_form = $(event.target).closest("form");
 
-		// No assignement for any user ?
-		if ( $("#issue_assigned_to_id").val() == '' ) {
-			// Kill event
-			event.preventDefault();
+        // No assignement for any user ?
+        if ($("#issue_assigned_to_id").val() == '') {
+            // Kill event
+            event.preventDefault();
 
-			// Open dialog
-			$( "#dialog-confirm" ).dialog({
-				resizable: false,
-				height:210,
-				width:350,
-				modal: true,
-				buttons: {
-					"Create ticket": function() {
-						the_issue_form.submit();
-					},
-					"Cancel": function() {
-						$( this ).dialog( "close" );
-					}
-				}
-			});
-		}
-	});
+            // Set right text for dialog
+            $('#dynamic-text').text('Create a ticket with no assigned person, not alert person (no email). Your ticket may go to the trap-door. We recommend that you set one!');
+
+            // Open dialog
+            $("#rah-dialog").dialog({
+                resizable: false,
+                height: 210,
+                width: 350,
+                modal: true,
+                buttons: {
+                    "Create ticket": function () {
+                        the_issue_form.submit();
+                    },
+                    "Cancel": function () {
+                        $(this).dialog("close");
+                    }
+                }
+            });
+        }
+    });
 });
